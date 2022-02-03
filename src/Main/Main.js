2e687f80cd6af4c8b1947e34d90fa29f7305a9f0
mariklim <maria.klimenko889@gmail.com>
2022-02-03 08:59:34 +0100

    add articles.body

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,14 @@
 import './Main.css'
 import articles from '../data/articles.json'
-function Main(){
+function Main() {
     return (
         <main className="Main-site-main">
-            {articles.map(item=><h2>{item.title}</h2>)}
+            {articles.map(item =>
+                <section>
+                    <h2>{item.title}</h2>
+                    <p>{item.body}</p>
+                </section>
+            )}
         </main>
     );
 }
