69b2052268cad6fbfc45908f5e4bf3ff649000c2
mariklim <maria.klimenko889@gmail.com>
2022-02-03 08:21:23 +0100

    add componets: App, Header, Footer

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import Header from '../Header/Header'
+import Footer from './Footer/Footer';
+import './App.css';
+
+function App() {
+    return(
+      <div className='container'>
+          <Header></Header>
+          <Footer/>
+      </div>  
+    );
+    
+}
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+function Header() {
+    return (
+        <header className="Header-site-header">
+            <div>
+                <h1>React site</h1>
+                <h2>Let's go!</h2>
+            </div>
+        </header>
+    );
+}
+export default Header;
